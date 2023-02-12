81eeb21265671e55f35cb0768314ac8c0f51693e
oclay <ocleidyreve@gmail.com>
2023-02-11 21:27:51 -0500

    Improve the parser fo the expressions

--- server/src/main/java/dev/oclay/evaluator/Evaluator.java
+++ server/src/main/java/dev/oclay/evaluator/Evaluator.java
@@ -1,7 +1,6 @@
 package dev.oclay.evaluator;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.regex.Pattern;
 
@@ -20,29 +19,31 @@ public class Evaluator {
         return validationPattern.matcher(expression).matches();
     }
 
-    private List<String> parse()  {
+    private String [] parse()  {
         StringBuilder currentNumber = new StringBuilder();
-        List<String> expressionValues = new ArrayList<>();
         char [] data = expression.toCharArray();
+        String [] expressionValues = new String[data.length];
+        int index = 0;
         for (char datum : data) {
             if (Character.isDigit(datum)) {
                 currentNumber.append(datum);
             } else {
-                expressionValues.add(currentNumber.toString());
+                expressionValues[index] = currentNumber.toString();
                 currentNumber.setLength(0);
-                expressionValues.add(String.valueOf(datum));
+                expressionValues[index+1] = Character.toString(datum);
+                index+=2;
             }
         }
-        expressionValues.add(currentNumber.toString());
-        return expressionValues;
+        expressionValues[index] = currentNumber.toString();
+        return Arrays.copyOf(expressionValues, index+1);
     }
 
     public int evaluate() {
         var expressionValues = parse();
-        Integer currentValue = Integer.parseInt(expressionValues.get(0));
-        for (int i = 0; i < expressionValues.size() - 1; i+=2) {
-            String operation = expressionValues.get(i+1);
-            Integer secondValue = Integer.parseInt(expressionValues.get(i+2));
+        Integer currentValue = Integer.parseInt(expressionValues[0]);
+        for (int i = 0; i < expressionValues.length - 1; i+=2) {
+            String operation = expressionValues[i+1];
+            Integer secondValue = Integer.parseInt(expressionValues[i+2]);
             currentValue = evalOperation(currentValue, operation, secondValue);
         }
         return currentValue;
